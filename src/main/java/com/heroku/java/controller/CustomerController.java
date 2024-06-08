90d8df3a67a42de15353eb7010c82775ad558fe9
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-08 16:39:41 +0800

    heroku gay

--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -1,26 +1,16 @@
 package com.heroku.java.controller;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.heroku.java.model.*;
-
-import jakarta.servlet.http.HttpSession;
-
-import java.sql.*;
-import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.Map;
-
-import java.util.List;
+import com.heroku.java.model.customer;
 
 @Controller
 public class CustomerController {
