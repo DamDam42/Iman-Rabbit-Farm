4f09a61937fdd2007cde63e14b8c60d0441a51bf
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-08 17:38:14 +0800

    a

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -11,9 +11,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.HttpSession;
 
 @SpringBootApplication
 @Controller
@@ -27,106 +24,12 @@ public class GettingStartedApplication {
 
 // for guest
 
-    @GetMapping("/")
-    public String index() {
-        return "index_logout";
-    }
-
-    @GetMapping("/index2")
-    public String index2() {
-        return "account/index2";
-    }
-
-    @GetMapping("/logoutViewRoom")
-    public String logoutViewRoom() {
-        return "logoutViewRoom";
-    }
-
-    @GetMapping("/index")
-    public String index1(@RequestParam(name = "success", required = false) Boolean success, HttpSession session) {
-        String guestICNumber = (String) session.getAttribute("guestICNumber");
-        return "index";
-    }
-
-    @GetMapping("/guestLogin")
-    public String guestLogin() {
-        return "guest/guestLogin";
-    }
-
-    @GetMapping("/index_logout")
-    public String index_logout() {
-        return "index_logout";
-    }
-
-        @GetMapping("/guestViewRoom")
-    public String guestViewRoom(HttpSession session) {
-        String guestICNumber = (String) session.getAttribute("guestICNumber");
-        return "guest/guestViewRoom";
-    }
-
-    // for staff
-
-    @GetMapping("/staffHome")
-    public String staffHome(@RequestParam(name = "success", required = false) Boolean success, HttpSession session) {
-        String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-        return "staff/staffHome";
-    }
-
-    @GetMapping("/staffLogin")
-    public String staffLogin() {
-        return "staff/staffLogin";
-    }
-
-    @GetMapping("/staffAddRoom")
-    public String staffAddRoom(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "staff/staffAddRoom";
+@GetMapping("/index2")
+public String index2() {
+    return "account/index2";
 }
 
-@GetMapping("/staffGenerateReport")
-public String staffGenerateReport(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "staff/staffGenerateReport";
-}
-
-// for manager
-
-// @GetMapping("/managerHome")
-// public String managerHome(@RequestParam(name = "success", required = false) Boolean success, HttpSession session) {
-//     String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-//     return "manager/managerHome";
-// }
-
-@GetMapping("/managerAddRoom")
-public String managerAddRoom(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "manager/managerAddRoom";
-}
-
-@GetMapping("/managerAddStaff")
-public String managerAddStaff(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "manager/managerAddStaff";
-}
-
-@GetMapping("/managerGenerateReport")
-public String managerGenerateReport(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "manager/managerGenerateReport";
-}
-
-@GetMapping("/reservationReport")
-public String reservationReport(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "manager/reservationReport";
-}
-
-@GetMapping("/salesReport")
-public String salesReport(HttpSession session) {
-    String staffICNumber = (String) session.getAttribute("staffICNumber") ;
-    return "manager/salesReport";
-}
-
+   
 
     @GetMapping("/database")
     String database(Map<String, Object> model) {
