64b743e1ef1f22b8c4772f8b37acf5c39aee6970
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-09 23:29:01 +0800

    a

--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -1,12 +1,12 @@
 package com.heroku.java.controller;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,45 +14,27 @@ import com.heroku.java.model.customer;
 
 @Controller
 public class CustomerController {
+
     private final DataSource dataSource;
 
     @Autowired
     public CustomerController(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    
-   
-
 
     @PostMapping("/customerRegister")
-    public String customerRegister(@ModelAttribute("customerRegister") customer customer, Model model) {
-
+    public String customerRegister(@ModelAttribute("customer") customer customer) {
         try (Connection connection = dataSource.getConnection()){
-            String sql = "INSERT INTO public.customer(custname,custpassword,custemail,custphonenum,custaddress) VALUES (?, ?, ?, ?, ?);";
-            final var statement = connection.prepareStatement(sql);
-            
-            String custName = customer.getCustName();
-            String custPassword = customer.getCustPassword();
-            String custEmail = customer.getCustEmail();
-            String custPhonenum = customer.getCustPhoneNum();
-            String custAddress = customer.getCustAddress();
-
-
-            statement.setString(1, custName);
-            statement.setString(2, custPassword);
-            statement.setString(3, custEmail);
-            statement.setString(4, custPhonenum);
-            statement.setString(5, custAddress);
-            
+            String sql = "INSERT INTO public.customer(custname, custpassword, custemail, custphonenum, custaddress) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, customer.getCustName());
+            statement.setString(2, customer.getCustPassword());
+            statement.setString(3, customer.getCustEmail());
+            statement.setString(4, customer.getCustPhoneNum());
+            statement.setString(5, customer.getCustAddress());
             statement.executeUpdate();
-
-            System.out.println("name : " + custName);
-            // System.out.println("type : "+protype);
-            // System.out.println("product price : RM"+proprice);
-            // System.out.println("proimg: "+proimgs.getBytes());
-
+            System.out.println("Name: " + customer.getCustName());
             connection.close();
-
         } catch (Exception e) {
             e.printStackTrace();
             return "redirect:/index";
