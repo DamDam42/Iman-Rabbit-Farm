03f0441157d6e48a1d7d8a54ddc6b2d7e5e77956
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-10 19:55:24 +0800

    a

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.heroku.java.model.Customer;
 
 @SpringBootApplication
 @Controller
@@ -26,11 +23,7 @@ public class GettingStartedApplication {
     }
 
 // for guest
-@GetMapping("/customerRegister")
-public String customerRegister(Model model) {
-     model.addAttribute("customerRegister", new Customer());
-    return "account/customerRegister";
-}
+
    
 
     @GetMapping("/database")
--- src/main/java/com/heroku/java/controller/CustomerController.java
+++ src/main/java/com/heroku/java/controller/CustomerController.java
@@ -7,6 +7,8 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,6 +23,12 @@ public class CustomerController {
     public CustomerController(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    @GetMapping("/customerRegister")
+public String customerRegister(Model model) {
+     model.addAttribute("customerRegister", new Customer());
+    return "account/customerRegister";
+}
 
     @PostMapping("/customerRegister")
     public String customerRegister(@ModelAttribute("customerRegister") Customer customer) {
